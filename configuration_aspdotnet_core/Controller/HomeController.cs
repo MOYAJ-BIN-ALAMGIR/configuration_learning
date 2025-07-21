5d53ec227f808ad5b17c14e63ecfb258c22906d9
Moyaj Bin Alamgir <130137627+MOYAJ-BIN-ALAMGIR@users.noreply.github.com>
2025-07-21 16:25:41 +0600

    Add HomeController and TotalUsers service
    Implemented HomeController to return total users count.
    Updated Program.cs to register TotalUsers as a singleton
    and set up routing. Modified Index.cshtml to use string model
    for displaying user count. Created TotalUsers class with
    method to generate random user count.

---
+++ configuration_aspdotnet_core/Controller/HomeController.cs
@@ -0,0 +1,18 @@
+﻿using Configuation.Services;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Configuation.Controllers
+{
+    public class HomeController : Controller
+    {
+        private TotalUsers totalUesrs;
+        public HomeController(TotalUsers tu)
+        {
+            totalUesrs = tu;
+        }
+        public String Index()
+        {
+            return "Total Users: " + totalUesrs.TUsers();
+        }
+    }
+}
--- configuration_aspdotnet_core/Program.cs
+++ configuration_aspdotnet_core/Program.cs
@@ -1,6 +1,26 @@
+using Configuation.Services;
+
 var builder = WebApplication.CreateBuilder(args);
+
+builder.Services.AddControllersWithViews();
+
+builder.Services.AddSingleton<TotalUsers>();
+
 var app = builder.Build();
 
-app.MapGet("/", () => "Hello World!");
+if(!app.Environment.IsDevelopment())
+{
+    app.UseExceptionHandler("/Home/Error");
+    app.UseHsts();
+}
+
+app.UseHttpsRedirection();
+app.UseStaticFiles();
+
+app.UseRouting();
+app.UseAuthorization();
+app.MapControllerRoute(
+    name: "defult",
+    pattern: "{controller=home}/{action=Index}/{id?}");
 
 app.Run();
