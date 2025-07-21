2435c4585f32c12f100b74d41c6193c2057ab29b
Moyaj Bin Alamgir <130137627+MOYAJ-BIN-ALAMGIR@users.noreply.github.com>
2025-07-21 18:10:37 +0600

    Update project configuration and clean up files
    - Enhance `.gitignore` to exclude build artifacts and caches.
    - Fix namespace in `ContentMiddleware.cs` and update constructor.
    - Remove unnecessary assembly info and cache files.
    - Streamline `GeneratedMSBuildEditorConfig.editorconfig`.
    - Delete obsolete project dependency and asset files.

--- configuration_aspdotnet_core/Middlewares/ContentMiddleware.cs
+++ configuration_aspdotnet_core/Middlewares/ContentMiddleware.cs
@@ -1,15 +1,22 @@
 ﻿//namespace configuration_aspdotnet_core.Middlewares
+using Configuation.Services;
+
 namespace Configuation.Middlewares
 {
     public class ContentMiddleware
     {
         private RequestDelegate nextDelegate;
-        public ContentMiddleware(RequestDelegate next) => nextDelegate = next;
+        private TotalUsers totalusers;
+        public ContentMiddleware(RequestDelegate next,TotalUsers tu)
+        {
+            nextDelegate = next;
+            totalusers = tu;
+        }
         public async Task Invoke(HttpContext httpContext)
         {
             if(httpContext.Request.Path.ToString()=="/middleware")
             {
-                await httpContext.Response.WriteAsync("This is from the content middleware.");
+                await httpContext.Response.WriteAsync("This is from the content middleware, Totall Users:"+totalusers.TUsers());
             }
             else
             {
