812c914b70ab45e52593078c0aaee3b6a85f0f36
Moyaj Bin Alamgir <130137627+MOYAJ-BIN-ALAMGIR@users.noreply.github.com>
2025-07-21 17:37:52 +0600

    Add ContentMiddleware and update project configuration
    - Introduced `ContentMiddleware` to handle requests at `/middleware`.
    - Updated `Program.cs` to include the new middleware in the pipeline.
    - Corrected namespace from `configuration_aspdotnet_core.Middlewares` to `Configuation.Middlewares`.
    - Added `Newtonsoft.Json` package reference in the project file.

---
+++ configuration_aspdotnet_core/Middlewares/ContentMiddleware.cs
@@ -0,0 +1,20 @@
+﻿//namespace configuration_aspdotnet_core.Middlewares
+namespace Configuation.Middlewares
+{
+    public class ContentMiddleware
+    {
+        private RequestDelegate nextDelegate;
+        public ContentMiddleware(RequestDelegate next) => nextDelegate = next;
+        public async Task Invoke(HttpContext httpContext)
+        {
+            if(httpContext.Request.Path.ToString()=="/middleware")
+            {
+                await httpContext.Response.WriteAsync("This is from the content middleware.");
+            }
+            else
+            {
+                await nextDelegate.Invoke(httpContext);
+            }
+        }
+    }
+}
